525956aa5b21fe6d36199ac3024cd66a3166612c
joao victor da silva cardoso <pealq63@gmail.com>
2024-05-15 18:51:23 -0300

    carrinho de compra funcionado

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let valorfinal = 0;
+
 function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
@@ -5,9 +7,14 @@ function adicionar() {
     let qtd = parseInt(document.getElementById('quantidade').value); 
 
     let preco = valorProduto * qtd;
-
+    
     let carrinho = document.getElementById('container__item');
     carrinho.innerHTML = carrinho.innerHTML + `<section class="container__itens__pedido">
     <span class="texto__produto"> ${qtd} </span> ${nomeProduto} <span class="texto__produto">R$${valorProduto}</span>
   </section>`
+    
+    valorfinal = valorfinal + preco;
+    compra = document.getElementById('valor__compra')
+    compra.textContent = `R$ ${valorfinal}`
+    document.getElementById('valor__compra').value = 0;
 }
